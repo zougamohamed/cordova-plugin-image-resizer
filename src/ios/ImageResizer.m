257fcc05b7027af350d6898dcc6416d2c923f771
Zouga Mohamed <zougamohamed@gmail.com>
2018-04-08 20:48:45 +0200

    Resize without keeping Ratio (Android)

--- src/ios/ImageResizer.m
+++ src/ios/ImageResizer.m
@@ -28,8 +28,6 @@ static NSInteger count = 0;
     CGSize frameSize = CGSizeMake([[arguments objectForKey:@"width"] floatValue], [[arguments objectForKey:@"height"] floatValue]);
     NSString* fileName = [arguments objectForKey:@"fileName"];
 
-    BOOL asBase64 = [[arguments objectForKey:@"base64"] boolValue];
-
     //    //Get the image from the path
     NSURL* imageURL = [NSURL URLWithString:imageUrlString];
 
@@ -51,38 +49,17 @@ static NSInteger count = 0;
 
     }];
 
-    NSLog(@"image resizer:%@",  (sourceImage ? @"image exists" : @"null" ));
+    NSLog(@"image resizer:%@",  (sourceImage  ? @"image exsist" : @"null" ));
 
     UIImage *tempImage = nil;
     CGSize targetSize = frameSize;
+    UIGraphicsBeginImageContext(targetSize);
 
     CGRect thumbnailRect = CGRectMake(0, 0, 0, 0);
     thumbnailRect.origin = CGPointMake(0.0,0.0);
+    thumbnailRect.size.width  = targetSize.width;
+    thumbnailRect.size.height = targetSize.height;
 
-    // get original image dimensions
-    CGFloat heightInPoints = sourceImage.size.height;
-    CGFloat heightInPixels = heightInPoints * sourceImage.scale;
-    CGFloat widthInPoints = sourceImage.size.width;
-    CGFloat widthInPixels = widthInPoints * sourceImage.scale;
-
-    // calculate the target dimensions in a way that preserves the original aspect ratio
-    CGFloat newWidth = targetSize.width;
-    CGFloat newHeight = targetSize.height;
-
-    if (heightInPixels > widthInPixels) {
-        // vertical image: use targetSize.height as reference for scaling
-        newWidth = widthInPixels * newHeight / heightInPixels;
-    } else {
-        // horizontal image: use targetSize.width as reference
-        newHeight = heightInPixels * newWidth / widthInPixels;
-    }
-
-    thumbnailRect.size.width  = newWidth;
-    thumbnailRect.size.height = newHeight;
-    targetSize.width = newWidth;
-    targetSize.height = newHeight;
-
-    UIGraphicsBeginImageContext(targetSize);
     [sourceImage drawInRect:thumbnailRect];
 
     tempImage = UIGraphicsGetImageFromCurrentImageContext();
@@ -90,24 +67,13 @@ static NSInteger count = 0;
 
     UIGraphicsEndImageContext();
     NSData *imageData = UIImageJPEGRepresentation(tempImage, [quality floatValue] / 100.0f );
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
-    NSString *cachesDirectory = [paths objectAtIndex:0];
-    BOOL isDir = NO;
-    NSError *error;
-    if (! [[NSFileManager defaultManager] fileExistsAtPath:cachesDirectory isDirectory:&isDir] && isDir == NO) {
-        [[NSFileManager defaultManager] createDirectoryAtPath:cachesDirectory withIntermediateDirectories:NO attributes:nil error:&error];
-    }
-    NSString *imagePath =[cachesDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"img%d.jpeg", count]];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *imagePath =[documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"img%d.jpeg",count]];
     count++;
     CDVPluginResult* result = nil;
 
-    if (asBase64) {
-        NSData *imageBase64 = [imageData base64EncodedDataWithOptions:NSDataBase64Encoding64CharacterLineLength];
-        NSString *imageBase64String = [[NSString alloc] initWithData:imageBase64 encoding:NSUTF8StringEncoding];
-        NSString *imageBase64URL = [NSString stringWithFormat:@"%@%@", @"data:image/jpeg;base64,", imageBase64String];
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:imageBase64URL];
-    }
-    else if (![imageData writeToFile:imagePath atomically:NO])
+    if (![imageData writeToFile:imagePath atomically:NO])
     {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:@"error save image"];
     }
